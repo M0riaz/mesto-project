dddebb383ed7d7e292ffa2fc83b04168f727a997
M0riaz <118534241+M0riaz@users.noreply.github.com>
2023-01-13 17:54:00 +0300

    логика для добавления новых картинок

--- script.js
+++ script.js
@@ -1,73 +1,156 @@
-//функция закрывающая окошко popup
-function popupClose () {
-  let close = document.querySelector('.popup_opened');
-  close.style.display = 'none';
-}
-let popupCloseButton = document.querySelector('.popup__toggle');
-popupCloseButton.addEventListener('click',popupClose );
-
-//функция открывающая окошко popup
-let profileButtonOpenClose = document.querySelector('.profile__editor');
-function popupOpen (){
-    let open = document.querySelector('.popup_opened');
-    open.style.display = 'flex';
-}
-profileButtonOpenClose.addEventListener('click', popupOpen)
-
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 //функция держащая popup закрытым до вызова по кнопке
-function popuphidden(){
+function popupHidden() {
     let popup = document.querySelector('.popup_opened');
     popup.style.display = 'none';
 }
-popuphidden();
+popupHidden();
+//функция держащая popup-image закрытым до вызова по кнопке
+function popupImageHidden(){
+    let popupImage = document.querySelector('.popup_opened-image');
+    popupImage.style.display = 'none';
+}
+popupImageHidden();
 
-// Находим форму в DOM
+
+//функция закрывающая окошко popup
+function popupClose() {
+    let close = document.querySelector('.popup_opened');
+    close.style.display = 'none';
+}
+
+const popupCloseButton = document.querySelector('.popup__toggle');
+popupCloseButton.addEventListener('click', popupClose);
+
+//функция открывающая окошко popup
+const profileButtonOpenClose = document.querySelector('.profile__editor');
+
+function popupOpen() {
+    let open = document.querySelector('.popup_opened');
+    open.style.display = 'flex';
+}
+
+profileButtonOpenClose.addEventListener('click', popupOpen)
+
+//редактирования профиля
 
 const formElement = document.querySelector('.popup__admin');
-// Воспользуйтесь методом querySelector()
-
-// Находим поля формы в DOM
-
-let nameInput = document.querySelector('.popup__item_value_name');
-// Воспользуйтесь инструментом .querySelector()
-
-let jobInput = document.querySelector('.popup__item_value_hobby');
-// Воспользуйтесь инструментом .querySelector()
+const nameInput = document.querySelector('.popup__item_value_name');
+const jobInput = document.querySelector('.popup__item_value_hobby');
 
 
-// Обработчик «отправки» формы, хотя пока
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let name = document.querySelector('.profile__name');
+    name.textContent = nameInput.value;
 
-// она никуда отправляться не будет
+    let job = document.querySelector('.profile__status');
+    job.textContent = jobInput.value;
 
-    function formSubmitHandler (evt) {
-        evt.preventDefault();
-// Эта строчка отменяет стандартную отправку формы.
+    let popup = document.querySelector('.popup_opened');
+    popup.style.display = 'none';
 
-
-// Так мы можем определить свою логику отправки.
-
-
-// О том, как это делать, расскажем позже.
-
-
-
-// Получите значение полей jobInput и nameInput из свойства value
-let name = document.querySelector('.profile__name');
-name.innerHTML = nameInput.value;
-
-let job = document.querySelector('.profile__status');
-job.innerHTML = jobInput.value;
-
-// Выберите элементы, куда должны быть вставлены значения полей
-
-
-
-// Вставьте новые значения с помощью textContent
-
-    }
-
-// Прикрепляем обработчик к форме:
-
-// он будет следить за событием “submit” - «отправка»
-
+}
 formElement.addEventListener('submit', formSubmitHandler);
+
+//функция открывающая окошко добавление картинки
+function popupOpenedImage(){
+    let open = document.querySelector('.popup_opened-image');
+    open.style.display = 'flex';
+}
+const buttonAddImageProfile = document.querySelector('.profile__button');
+buttonAddImageProfile.addEventListener('click',popupOpenedImage);
+
+//функция закрывающая попап с добавлением картинок
+function closePopupImage(){
+    let closeButton = document.querySelector('.popup_opened-image');
+    closeButton.style.display = 'none';
+}
+const buttonClosePopupImage = document.querySelector('.popup__toggle-close');
+buttonClosePopupImage.addEventListener('click',closePopupImage);
+
+//функция добавляющая картинки на страницу через popup img
+
+
+
+// function imageAdd(linkInput, newPlaceInput) {
+//     const cardTeamplate = document.querySelector('.card-teamplate').content;
+//     const cardElement =cardTeamplate.querySelector('.elements__card').cloneNode(true);
+//
+//     cardElement.querySelector('.elements__image').textContent = linkInput;
+//     cardElement.querySelector('.elements__title').textContent = newPlaceInput;
+//
+//     elementsList.prepend(cardElement);
+// }
+//
+// const createButton = document.querySelector('.popup__button-create');
+//
+// createButton.addEventListener('click', function () {
+//     const newPlaceInput = document.querySelector('.popup__item_value_new-place');
+//     const linkInput = document.querySelector('.popup__item_value_link');
+//
+//     imageAdd(newPlaceInput.value,linkInput.value)
+//     console.log(newPlaceInput)
+// })
+
+
+// function add(addBlock){
+//     let cardTeam = document.querySelector('.card-teamplate').content;
+//     let cardElem = cardTeam.querySelector('.elements__card').cloneNode(true);
+//
+//     elementsList.prepend(cardElem)
+// }
+
+const elementsList = document.querySelector('.elements__list');
+
+function addImage(imageValue,titleValue) {
+    let cardTeam = document.querySelector('#card-template').content;
+    let cardElem = cardTeam.querySelector('.elements__card').cloneNode(true);
+
+    cardElem.querySelector('.elements__image').textContent = imageValue;
+    cardElem.querySelector('.elements__title').textContent = titleValue;
+
+
+    elementsList.prepend(cardElem);
+}
+
+function formSubmit(evt) {
+    evt.preventDefault();
+    let linkInput = document.querySelector('.popup__item_value_link');
+    let placeInput = document.querySelector('.popup__item_value_new-place');
+
+    addImage(linkInput.value,placeInput.value)
+
+    // let popup = document.querySelector('.popup_opened');
+    // popup.style.display = 'none';
+
+}
+
+const submitButton = document.querySelector('.popup__button-create')
+submitButton.addEventListener('click', formSubmit);
+
