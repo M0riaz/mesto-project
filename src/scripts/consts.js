90e35750ad883c847a961a3f222390d028e88336
M0riaz <118534241+M0riaz@users.noreply.github.com>
2023-02-15 12:34:36 +0300

    test

---
+++ src/scripts/consts.js
@@ -0,0 +1,34 @@
+//закрывающий элемент в popup
+export const popupCloseButton = document.querySelector('.popup__toggle-edit-button ');
+//элемент редактирования профиля(карандашик)
+export const profileButtonOpenClose = document.querySelector('.profile__editor');
+//форма новое место
+export const formNewPlace = document.querySelector('.popup__admin-new-place');
+//форма профиль
+export const formProfile = document.querySelector('.popup__admin');
+// элемент в popup имя
+export const nameInput = document.querySelector('.popup__item_value_name');
+// элемент в popup хобби
+export const jobInput = document.querySelector('.popup__item_value_hobby');
+// 'элемент большая кнопка в профиле на добавление карточек с картинками
+export const buttonAddImageProfile = document.querySelector('.profile__button');
+// элемент Ul (список карточек, основное хранилище)
+export const elementsList = document.querySelector('.elements__list');
+// элемент тимплейта
+export const cardTemplate = document.querySelector('.card-template').content;
+// кнопка закрытия у попапа с большой картинкой
+export const buttonCloseBigImage = document.querySelector('.popup__toggle-big-image');
+// кнопка закрытия у попапа с добавлением картинок
+export const popupAddImageClose = document.querySelector('.popup__toggle-add-image');
+// элемент профиля имя пользователя
+export const profileName = document.querySelector('.profile__name');
+// элемент профиля статус
+export const profileStatus = document.querySelector('.profile__status');
+// попап редактирования профиля и статуса
+export const popupProfileOpenClose = document.querySelector('.popup-profile')
+//попап редактирования большой картинки
+export const bigImagePopup = document.querySelector('.popup_opened-big-image')
+// попап редактирования добавления ссылки на картинку подписи с местом
+export const imagePopupOpen = document.querySelector('.popup_opened-image')
+// выбор всех попапов
+export const popupMain = document.querySelectorAll('.popup')
